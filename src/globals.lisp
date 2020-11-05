1a85d677b283071438a282904585aa7cff51ceb5
José A. Navarro Ramón <janr.devel@gmail.com>
2020-11-05 08:38:46 +0100

    Myemacs 0.1.0 alpha-1

--- src/globals.lisp
+++ src/globals.lisp
@@ -19,7 +19,7 @@
 (defparameter *progname* "myemacs")
 (defparameter *version* "0.1.0")
 (defparameter *production* "alpha-1")
-(defparameter *version-date* "04-11-2020")
+(defparameter *version-date* "05-11-2020")
 
 ;;;
 (defparameter *author* "José A. Navarro Ramón")
